400c21e1565c29745819505be856ab70eae2358d
Raymond Yang <yangrunhua@gmail.com>
2023-02-08 11:47:10 +0800

    Initial submit

--- TopPopularLinksSpark.py
+++ TopPopularLinksSpark.py
@@ -8,12 +8,23 @@ sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+def mapper(line):
+    src, dest = line.strip().split(':', 1)
+    dest_list = [i.strip() for i in dest.split(' ') if i]
+    result = [(src.strip(), 0)]
+    for d in dest_list:
+        if d and not d == src.strip():
+            result.append((d, 1))
+    return result
 
-output = open(sys.argv[2], "w")
 
-#TODO
-#write results to output file. Foramt for each line: (key + \t + value +"\n")
+outputFile = open(sys.argv[2], "w", encoding='utf-8')
 
+output = lines.flatMap(mapper).reduceByKey(lambda x, y: x+y).top(10, lambda x: x[1]).sortBy(lambda x: x[0]).collect()
+
+for i in output:
+    outputFile.write(u"%s\t%i\n" % (i[0], i[i]))
+
+outputFile.close()
 sc.stop()
 
